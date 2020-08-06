c8ae27fb02c5b060711757b849604e108191d10b
Donald Cameron <drc85@live.co.uk>
2020-08-06 13:59:19 +0100

    initial client and server structure set up

---
+++ server/src/main/java/com/example/half_baked_app/HalfBakedAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.half_baked_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfBakedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalfBakedAppApplication.class, args);
+	}
+
+}

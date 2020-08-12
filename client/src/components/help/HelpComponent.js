9393e67e99e5244f85095a1eba82238b9c8aa7cf
Donald Cameron <drc85@live.co.uk>
2020-08-12 10:28:25 +0100

    updated help file with instructions

--- client/src/components/help/HelpComponent.js
+++ client/src/components/help/HelpComponent.js
@@ -20,8 +20,33 @@ const Help = (props) => {
 
       return (
         <div>
+        <h3>
+        How To use Half-Baked
+        </h3>
           <p>
-            Some information about the application, how to use etc will go in here.
+            Welcome to Half Baked!
+            The application to help you search for recipies to suit your needs.
+
+            Click on a recipe in the left hand bar to load the instructions and ingredients.
+
+            To navigate please follow the instructions below.
+          </p>
+          <p>
+            Click on Profile to bring up your user details. These may be updated at anytime.
+          </p>
+          <p>
+            Click on My Recipies to return a list of your saved recipies and blacklisted recipes.
+          </p>
+          <p>
+            Click on My Shopping List to display the items you require to purchase to complete your recipe.
+          </p>
+          <p>
+            Click on Dietary Needs and Intolerances to update your preferences.
+          </p>
+          <p>
+            Click on Search to bring up the search menu where you can filter recipies by your needs and intolerances.
+            Please note, at this time, you must correctly spell the desired ingredient and separate with a comma and NO spaces.
+            E.g. egg,bacon,bread
           </p>
         </div>
       )
